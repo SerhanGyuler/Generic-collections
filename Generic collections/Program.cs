e21c5ff9ce64fef3d480c6987c496b60cfb1b2ee
Serhan Gyuler <serhan.gyuler@hotmail.com>
2024-11-16 14:34:51 +0100

    Added constructor, dishes to list and show menu

--- Generic collections/Program.cs
+++ Generic collections/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Boring line
+            string lines = "--------------------";
+
+            // Constructor
+            Resturant resturant = new Resturant();
+
+            // Dish
+            resturant.AddtoMenu(new MenuItem(1, "Pizza", 99.90m));
+            resturant.AddtoMenu(new MenuItem(1, "Hamburger", 79.90m));
+            resturant.AddtoMenu(new MenuItem(1, "Pasta", 89.90m));
+            resturant.AddtoMenu(new MenuItem(1, "Tacos", 79.90m));
+
+            // Create Orders
+            
+
+            // Show Menu
+            Console.WriteLine($"{lines}\nMenu:");
+            resturant.ShowMenu();
+            
         }
     }
 }
