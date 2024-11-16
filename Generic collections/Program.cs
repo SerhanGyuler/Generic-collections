07d7a30c8230e34bc1f6e0a933a51781b4438252
Serhan Gyuler <serhan.gyuler@hotmail.com>
2024-11-16 12:57:42 +0100

    Add project files.

---
+++ Generic collections/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generic_collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
